b840540c602e8ee4b4935b3441f5e1b2985f25fa
Popov Philipp <philolippo@gmail.com>
2020-10-27 23:29:03 +0300

    Complete task 1

---
+++ lab_5/caesar.py
@@ -0,0 +1,87 @@
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+
+    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = list()
+
+    for char in plaintext:
+
+        if char.islower():
+            alphabet = lower_alphabet
+        else:
+            alphabet = upper_alphabet
+
+        if char not in alphabet:
+            result.append(char)
+            continue
+        char_index = alphabet.index(char)
+
+        new_index = char_index + shift
+
+        if new_index > 25:
+            new_index = new_index % 26
+
+        new_char = alphabet[new_index]
+        result.append(new_char)
+
+    ciphertext = "".join(result)
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+
+    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = list()
+
+    for char in ciphertext:
+
+        if char.islower():
+            alphabet = lower_alphabet
+        else:
+            alphabet = upper_alphabet
+
+        if char not in alphabet:
+            result.append(char)
+            continue
+
+        char_index = alphabet.index(char)
+
+        new_index = char_index + (26 - shift)
+
+        if new_index > 25:
+            new_index = new_index % 26
+
+        new_char = alphabet[new_index]
+        result.append(new_char)
+
+    ciphertext = "".join(result)
+
+    return ciphertext
+
+
+if __name__ == "__main__":
+    print(encrypt_caesar("PYTHON"))
