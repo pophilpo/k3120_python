900e7b7158f5ef0ba0f8ea3ea04d5f6f99185b0b
Popov Philipp <philolippo@gmail.com>
2020-11-20 13:30:41 +0300

    Implement reading from file and getting row

--- lab_6/sudoku.cpp
+++ lab_6/sudoku.cpp
@@ -1,18 +1,34 @@
 #include "iostream"
 #include "vector"
 #include <fstream>
+#include <sys/types.h>
 
 using namespace std;
+
+struct Position {
+  int row;
+  int column;
+};
 
 class Grid {
 
 private:
   vector<vector<int>> storage;
+  vector<int> input_values;
 
 public:
+  void Group(int n) {
+    for (size_t i = 0; i < input_values.size(); i = i + n) {
+
+      vector<int> row =
+          vector<int>(input_values.begin() + i, input_values.begin() + (i + n));
+
+      storage.push_back(row);
+    }
+  }
+
   void ReadFromFile(string &filename) {
 
-    vector<int> full_data;
     ifstream file(filename);
 
     char c;
@@ -20,20 +36,17 @@ public:
 
       if (c == '.') {
 
-        full_data.push_back(0);
+        input_values.push_back(0);
       } else {
 
         int ichar = c - '0';
 
-        full_data.push_back(ichar);
+        input_values.push_back(ichar);
       }
     }
-
-    cout << full_data.size() << endl;
-    for (int value : full_data) {
-      cout << value << endl;
-    }
   }
+
+  vector<int> GetRow(Position position) { return storage[position.row]; }
 };
 
 int main() {
@@ -41,6 +54,13 @@ int main() {
   Grid grid;
   string filename = "puzzle1.txt";
   grid.ReadFromFile(filename);
+  grid.Group(9);
+
+  Position position = {4, 0};
+  vector<int> row = grid.GetRow(position);
+  for (int values : row) {
+    cout << values << " ";
+  }
 
   return 0;
 }
