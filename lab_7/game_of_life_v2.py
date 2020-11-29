0fbcf2576a6d76afaf59752088d7731e0c5f2332
Popov Philipp <philolippo@gmail.com>
2020-11-30 00:40:40 +0300

    Implement Console version of the game

--- lab_7/game_of_life_v2.py
+++ lab_7/game_of_life_v2.py
@@ -7,10 +7,12 @@ import random
 import sys
 import copy
 
+import time
+
 
 class GameOfLife:
 
-    def __init__(self, size, randomize: bool=True, max_generations=None) -> None:
+    def __init__(self, size, randomize: bool=True, max_generations=None, speed=100) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
         # Предыдущее поколение клеток
@@ -25,6 +27,8 @@ class GameOfLife:
         self.generations = 1
 
         self.clock = pygame.time.Clock()
+
+        self.speed = speed
 
     def create_grid(self, randomize: bool=False):
         grid = list()
@@ -169,36 +173,76 @@ class GameOfLife:
         print("Successfully exported current grid state.")
 
 
-
-
 class UI(abc.ABC):
 
-    def __init__(self, life: GameOfLife):
+    def __init__(self, life):
         self.life = life
-    
-    @abc.abstractmethod
 
+    @abc.abstractmethod
     def run(self):
         pass
-    
+
 
 class Console(UI):
-
     def __init__(self, life):
         super().__init__(life)
+
+
+    def draw_borders(self, screen):
+
+        begin_x = 0
+        begin_y = 0
+
+        # Adding plus 3 so that points don't cross borders
+        height = self.life.rows + 3
+        width = self.life.cols + 3
+
+        grid = screen.subwin(height,width, begin_x, begin_y)
+        grid.box()
+        screen.getch()
     
-    def draw_borders(self, screen)
+
+    def draw_grid(self, screen):
+
+        for x, row in enumerate(self.life.curr_grid):
+
+            for y, value in enumerate(row):
+
+                pos_x = x + 1
+                pos_y = y + 1
 
 
-    
+                if value == 1:
+                    screen.addch(pos_y, pos_x, "O")
+                else:
+                    screen.addch(pos_y, pos_x, ".")
 
+        screen.refresh()
+
+
+
+
+
+
+    def run(self):
+
+        screen = curses.initscr()
+        self.draw_borders(screen)
+
+        self.draw_grid(screen)
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            self.life.step()
+            self.draw_grid(screen)
+            curses.napms(self.life.speed)
+
+
+        curses.endwin()
 
 def main():
 
-    life = GameOfLife.from_file("gameoflife.txt")
-
-
-
+    life = GameOfLife((50, 50), True, 100, 10)
+    ui = Console(life)
+    ui.run()
 
 
 if __name__ == "__main__":
